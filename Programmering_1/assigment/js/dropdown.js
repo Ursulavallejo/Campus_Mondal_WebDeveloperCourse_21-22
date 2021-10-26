4b3594f969c4d7681448ca70f23bb04dfe82ab46
ursula_vallejo <ursulavallejo@gmail.com>
2021-10-26 23:48:54 +0200

    dropmenu

--- Programmering_1/assigment/js/dropdown.js
+++ Programmering_1/assigment/js/dropdown.js
@@ -1,38 +1,3 @@
-function createDropMenu (){
-    return`
-    <section class="dropdown">
-            <div class="checkOut">
-              <img src="img/shoppingCart.png" alt="cartBuy" />
-              <button class="checkoutDropdownButton" onclick="openMenu()">
-                Checkout
-              </button>
-            </div>
-            <div id="dropdown" class="dropdown-content">
-              <h2>Your Cart</h2>
-              <hr />
-
-              <section class="grid-container-ShopCart">
-                <article class="grid-item1 area-item1">side</article>
-                <article id="cartProducts" class="grid-item1 area-item2">
-                  top
-                </article>
-                <article class="grid-item1 area-item3">
-                  <p>
-                    SUM(<span class="totalCds"></span>)
-                    <span id="totalPay">SEK</span>
-                  </p>
-                </article>
-
-                <button onclick="closeMenu()">Close</button>
-                <button onclick="toCheckout()">Checkout</button>
-              </section>
-            </div>
-          </section>
-    
-    `
-}
-
-/*
 function openMenu() {
     document.getElementById('dropdown')
         .classList.toggle('show')
@@ -44,4 +9,4 @@ function closeMenu() {
 }
 function toCheckout(){
     alert ('Thank you for visiting us!! At the moment we cant offer to buy any product!')
-}*/
+}
--- Programmering_1/assigment/js/header.js
+++ Programmering_1/assigment/js/header.js
@@ -1,5 +1,5 @@
-function createContentHeader(){
-    return`
+function createContentHeader() {
+    return `
 
    <section class="grid-container-header">
           <div class="logo">
@@ -18,6 +18,28 @@ function createContentHeader(){
                 Checkout
               </button>
             </div>
+            <div id="dropdown" class="dropdown-content">
+              <h2>Your Cart</h2>
+              <hr />
+
+              <section class="grid-container-ShopCart">
+                <article class="grid-item1 area-item1">side</article>
+                <article id="cartProducts" class="grid-item1 area-item2">
+                  top
+                </article>
+                <article class="grid-item1 area-item3">
+                  <p>
+                    SUM(<span class="totalCds"></span>)
+                    <span id="totalPay">SEK</span>
+                  </p>
+                </article>
+
+                <button onclick="closeMenu()">Close</button>
+                <button onclick="toCheckout()">Checkout</button>
+              </section>
+            </div>
+          </div>
 `
 }
+
 document.getElementById('header').innerHTML = createContentHeader()

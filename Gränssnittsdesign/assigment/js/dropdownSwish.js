95ca0cc177c060dde2f73aaf70efab2b60c80dd6
ursula_vallejo <ursulavallejo@gmail.com>
2021-12-29 10:26:00 +0100

    swish dropmenu

---
+++ Gränssnittsdesign/assigment/js/dropdownSwish.js
@@ -0,0 +1,10 @@
+function openMenu() {
+// opens dropdown Menu
+    document.getElementById('dropdown').classList.toggle('show')
+
+}
+
+function closeMenu() {
+// closes dropdown Menu
+    document.getElementById('dropdown').classList.remove('show')
+}
